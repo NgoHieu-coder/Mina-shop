0701c862562f2e6962fbbd66cb4c8d7853acee36
pnhoanganh <pnhoanganhh@gmail.com>
2024-06-21 13:40:29 +0700

    #1, #3, #15, #16 - pnhoanganh responsive header, footer, home page, shop page

--- scripts.js
+++ scripts.js
@@ -1,20 +1,33 @@
-const submitEmailBtn = document.getElementById('submitEmail'); 
-const emailInput = document.getElementById('email'); 
-const emailAlert = document.getElementById('alertEmail'); 
-console.log(emailInput.value);
+const submitEmailBtn = document.getElementById("submitEmail");
+const emailInput = document.getElementById("email");
+const emailAlert = document.getElementById("alertEmail");
+const menu = document.getElementById("nav__menu");
+const openButton = document.getElementById("navToggle");
+const menuIcon = document.querySelector("#navToggle i");
+console.log(menu);
+openButton.addEventListener("click", () => {
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+  if (menu.style.display === "block") {
+    menuIcon.classList.remove("fa-bars", "nav__burger");
+    menuIcon.classList.add("fa-xmark", "nav__close");
+  } else {
+    menuIcon.classList.add("fa-bars", "nav__burger");
+    menuIcon.classList.remove("fa-xmark", "nav__close");
+  }
+});
 
-submitEmailBtn.addEventListener('click', () => { 
-    const email = emailInput.value; 
-    if (email.length === 0) { 
-        submitEmailBtn.style.transform = 'translateY(32px)'; 
-        emailAlert.style.opacity = '1'; 
-    } 
-})
+submitEmailBtn.addEventListener("click", function () {
+  const email = emailInput.value;
+  if (email.length === 0) {
+    submitEmailBtn.style.transform = "translateY(10px)";
+    emailAlert.style.opacity = "1";
+  }
+});
 
-emailInput.addEventListener('input', (e) => { 
-    const email = e.target.value; 
-   if (email.length > 0) { 
-       submitEmailBtn.style.transform = 'translateY(0)'; 
-       emailAlert.style.opacity = '0'; 
-   }
-})
+emailInput.addEventListener("input", function (e) {
+  const email = e.target.value;
+  if (email.length > 0) {
+    submitEmailBtn.style.transform = "translateY(0)";
+    emailAlert.style.opacity = "0";
+  }
+});
