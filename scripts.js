759c866ea6d6dbbb46413f73a056f91b52546079
pnhoanganh <pnhoanganhh@gmail.com>
2024-06-23 00:59:28 +0700

    #3 - pnhoanganh add cart box

--- scripts.js
+++ scripts.js
@@ -7,8 +7,16 @@ const menuIcon = document.querySelector("#navToggle i");
 const dropdown = document.getElementById("dropdown");
 const subNavList = document.getElementById("subNavList");
 const subnav = document.querySelector(".subnav");
+const cartBox = document.querySelector(".cartBox");
+const navCart = document.querySelector(".navCart");
+const cartIcon = document.querySelector(".nav__shop");
+const closeIcon = document.querySelector(".fa-xmark");
+const overlay = document.querySelector(".cartBox-overlay");
 console.log(subNavList);
+
 openButton.addEventListener("click", () => {
+  cartBox.classList.remove("active");
+  overlay.style.display = "none";
   menu.style.display = menu.style.display === "block" ? "none" : "block";
   if (menu.style.display === "block") {
     menuIcon.classList.remove("fa-bars", "nav__burger");
@@ -50,4 +58,35 @@ if (window.innerWidth < 730) {
     }
     console.log(subnav.style.maxHeight);
   });
+
+  cartIcon.onclick = () => {
+    cartBox.classList.toggle("active");
+    menu.style.display = "none";
+    menuIcon.classList.add("fa-bars", "nav__burger");
+    menuIcon.classList.remove("fa-xmark", "nav__close");
+    if (cartBox.classList.contains("active")) {
+      overlay.style.display = "block";
+    } else {
+      overlay.style.display = "none";
+    }
+  };
 }
+
+navCart.onclick = () => {
+  cartBox.classList.toggle("active");
+  if (cartBox.classList.contains("active")) {
+    overlay.style.display = "block";
+  } else {
+    overlay.style.display = "none";
+  }
+};
+
+closeIcon.onclick = () => {
+  cartBox.classList.remove("active");
+  overlay.style.display = "none";
+};
+
+window.onscroll = () => {
+  cartBox.classList.remove("active");
+  overlay.style.display = "none";
+};
