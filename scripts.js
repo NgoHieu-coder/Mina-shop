7935ee57407fd78cbc16342a8558f9bc0b33c6f5
pnhoanganh <pnhoanganhh@gmail.com>
2024-06-26 23:51:37 +0700

    #3 - pnhoanganh add animation scroll down

--- scripts.js
+++ scripts.js
@@ -94,3 +94,22 @@ window.onscroll = () => {
   cartBox.classList.remove("active");
   overlay.style.display = "none";
 };
+
+//ANIMATION SCROLL DOWN
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 100;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
